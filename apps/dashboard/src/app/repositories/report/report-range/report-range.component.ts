231d4d968379607bbbdf00caf4b1d90fa9299238
José Prieto <jose.c.prieto.c2015@gmail.com>
2023-07-26 21:35:01 -0400

    ojala el ultimo

--- apps/dashboard/src/app/repositories/report/report-range/report-range.component.ts
+++ apps/dashboard/src/app/repositories/report/report-range/report-range.component.ts
@@ -22,7 +22,7 @@ export class ReportRangeComponent implements OnInit, OnDestroy {
   reportForm!: FormGroup;
   width = 460
   height = 400
-  margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  margin = { top: 40, right: 60, bottom: 30, left: 30 };
   data: any[] = []
   svg: any;
   maxDate = new Date(2100, 11, 31);
@@ -110,6 +110,28 @@ export class ReportRangeComponent implements OnInit, OnDestroy {
           .tickFormat(d3.format(".0f"))).selectAll("text")
         .style("font-size", "16px");
 
+      //
+      const gradient = this.svg.append("svg:defs")
+        .append("svg:linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "100%")
+        .attr("spreadMethod", "pad");
+
+      gradient.append("svg:stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "#19ab80")
+        .attr("stop-opacity", 1);
+
+      gradient.append("svg:stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "#0e9163")
+        .attr("stop-opacity", 1);
+
+      //
+
       this.svg.selectAll(".bar")
         .data(counts)
         .enter()
@@ -119,7 +141,22 @@ export class ReportRangeComponent implements OnInit, OnDestroy {
         .attr("y", (d: any) => y(d[1]))
         .attr("width", x.bandwidth())
         .attr("height", (d: any) => this.height - y(d[1]))
-        .attr("fill", "#19ab80");;
+        .attr("fill", "url(#gradient)");
+
+
+      this.svg.append("text")
+        .attr("transform", "translate(" + (this.width / 2) + " ," + (- 20) + ")")
+        .style("text-anchor", "middle")
+        .text("Especialidades")
+        .style("font-size", "18px");
+
+      this.svg.append("text")
+        .attr("transform", "rotate(90)")
+        .attr("x", (this.height / 2))
+        .attr("y", - (this.width + 5))
+        .style("text-anchor", "middle")
+        .text("Numero de Citas")
+        .style("font-size", "18px");
     }
   }
 }
