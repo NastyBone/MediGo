8e5d013a49fff6888d8737340792372a6488a64d
José Prieto <jose.c.prieto.c2015@gmail.com>
2023-07-30 18:15:50 -0400

    last

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,8 +1,20 @@
 const { composePlugins, withNx } = require('@nrwl/webpack');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const join = require('path').join;
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins = [
+    ...config.plugins,
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: join(__dirname, '..', '..', 'node_modules/tslib'),
+          to: 'tslib',
+        },
+      ],
+    }),
+  ];
   return config;
 });
